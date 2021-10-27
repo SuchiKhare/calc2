a578d39a123786a40101dec4c851f9a791b6dddc
SuchiKhare <sk368.njit.edu>
2021-10-27 00:21:39 -0400

    added space to check how travis will get affected

--- calculator/main.py
+++ calculator/main.py
@@ -8,7 +8,7 @@ class Calculator:
         return self.result
 
     def add_number(self, value_a):
-        """ adds number to result"""
+        """ adds number to result """
         self.result = self.result + value_a
         return self.result
     def subtract_number(self, value_a):
@@ -16,6 +16,6 @@ class Calculator:
         self.result = self.result - value_a
         return self.result
     def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
+        """ multiply two numbers and store the result """
         self.result = value_a * value_b
         return self.result
