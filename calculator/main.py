5d90280f1081d09daff771b9927a3388da3f1313
SuchiKhare <sk368.njit.edu>
2021-10-27 00:30:15 -0400

    added space to check how travis will get affected

--- calculator/main.py
+++ calculator/main.py
@@ -1,10 +1,10 @@
-""" This is the increment function"""
+""" This is the increment function """
 class Calculator:
-    """ This is the Calculator class"""
+    """ This is the Calculator class """
 
     result = 0
     def get_result(self):
-        """ Get Result of Calculation"""
+        """ Get Result of Calculation """
         return self.result
 
     def add_number(self, value_a):
@@ -12,7 +12,7 @@ class Calculator:
         self.result = self.result + value_a
         return self.result
     def subtract_number(self, value_a):
-        """ subtract number from result"""
+        """ subtract number from result """
         self.result = self.result - value_a
         return self.result
     def multiply_numbers(self, value_a, value_b):
