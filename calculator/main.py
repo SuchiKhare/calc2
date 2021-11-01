62f99a8409822ad6003ef3fa672fc96481f3402f
SuchiKhare <sk368.njit.edu>
2021-11-01 13:30:04 -0400

    3 static methods

--- calculator/main.py
+++ calculator/main.py
@@ -1,21 +1,23 @@
-""" This is the increment function """
+""" This is the increment function"""
+
+
 class Calculator:
     """ This is the Calculator class """
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation """
-        return self.result
+    @staticmethod
+    def add_number(value_a, value_b):
+        """ adds number """
+        total = value_a + value_b
+        return total
 
-    def add_number(self, value_a):
-        """ adds number to result """
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result """
-        self.result = value_a * value_b
-        return self.result
+    @staticmethod
+    def subtract_number(value_a, value_b):
+        """ subtract number """
+        total = value_a - value_b
+        return total
+
+    @staticmethod
+    def multiply_number(value_a, value_b):
+        """ multiply number """
+        total = value_a * value_b
+        return total
